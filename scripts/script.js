8f20d7798e7dcf0b9852aa67175f5ea3ae571b66
cornelvonwyl <45622562+cornelvonwyl@users.noreply.github.com>
2019-12-10 09:46:11 +0100

    cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-/*  function delay (URL) {
-    setTimeout( function() { window.location = URL }, 500 );
-}
-*/
-
-
 /*------------ SEITEN NAVIGATION VORWÄRTS PART 1 ------------*/
 var WhenButtonRightClicked = document.querySelector(".nextpage-right");
 WhenButtonRightClicked.addEventListener("click", function () {
@@ -11,11 +5,6 @@ WhenButtonRightClicked.addEventListener("click", function () {
     load.classList.add("change-page-forward");
 
     localStorage.setItem("whichAnimation", "fromright");
-
-    /*const GetClassesToRemove = document.querySelectorAll(".nextpage-left, .nextpage-right, .main-image");
-    for (let i = 0; i < GetClassesToRemove.length; i++) {
-        GetClassesToRemove[i].classList.add("displaynone-button-image");
-    }*/
 });
 
 
@@ -55,11 +44,6 @@ function myFunction(element) {
 }
 
 
-
-
-
-
-
 if (localStorage.getItem("whichAnimation") === "fromright") {
     const loader = document.querySelector(".loader-forward-nextpage");
     loader.classList.add("change-page-forward2");
@@ -81,97 +65,22 @@ if (localStorage.getItem("whichAnimation") === "fromleft") {
 
 
 
-/*window.addEventListener('scroll', () => {
-    if (isElementInViewport(tryout)) {
-        const animationfire = document.querySelector("#tryout");
-        animationfire.classList.add("resize");
-    }
-  })
-
-
-
-function isElementInViewport (el) {
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    )
-
-
-}*/
-
-/*
-window.addEventListener('scroll', () => {
-    if (isElementInViewport(tryout)) {
-        var animationfire = document.querySelectorAll(".loadanimation");
-
-        for(let i = 0; i < animationfire.length; i++){
-        animationfire[i].classList.add("loadandanimationit");
-        }
-    }
-  })
-
-
-
-function isElementInViewport (.el) {
-
-   for(let i = 0; i < 100; i++){
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    )
-
-}
-}*/
-
 var apps = document.querySelectorAll(".loadanimation"),
-   positions = [];
+    positions = [];
 
 window.addEventListener("scroll", (e) => {
-   apps.forEach(function (app, index) {
-       if (isInViewport(app)) {
-           apps[index].classList.add("loadandanimationit");
-       }
-   })
+    apps.forEach(function (app, index) {
+        if (isInViewport(app)) {
+            apps[index].classList.add("loadandanimationit");
+        }
+    })
 })
 
 var isInViewport = function (elem) {
-   var bounding = elem.getBoundingClientRect();
-   return (
-       bounding.top >= 0 &&
-       bounding.bottom <= window.innerHeight + bounding.height
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.bottom <= window.innerHeight + bounding.height
 
-   );
+    );
 };
-
-
-
-
-/*
-var rect = document.querySelector(".loadanimation").getBoundingClientRect();
-
-let top1 = rect.top;
-let bottom = rect.bottom;
-
-if (rect.top > 20) {
-    rect.classList.add("loadandanimationit")
-}
-
-console.log(rect);*/
-
-
-
-/*
-window.addEventListener("scroll", () => {
-    if (top1 > 0) {
-        const animationfire = document.querySelector("#tryout");
-        animationfire.classList.add("resize");
-    }
-})*/
